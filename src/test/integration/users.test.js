9d3419ea30c7ec59b1f4bbeca11477b481173b80
mcpablo <pablomconturso@gmail.com>
2024-11-16 09:56:18 -0300

    wip

--- src/test/integration/users.test.js
+++ src/test/integration/users.test.js
@@ -15,30 +15,31 @@ describe("Test de integración Users", () => {
       const newUser = {
         name: "Juan",
         surname: "PÉREZ",
-        email: "test@test.com",
+        email: "test@testfb.com",
         password: "coder123"
       };
       const { status, body } = await request.post("/").send(newUser);
       test_user = body.payload;
+      console.log(body);
       expect(status).to.be.equal(201);
       expect(body.payload).to.be.an("object");
       expect(body.payload.name).to.be.equal("JUAN");
       expect(body.payload.surname).to.be.equal("PÉREZ");
-      expect(body.payload.email).to.be.equal("test@test.com");
+      expect(body.payload.email).to.be.equal("test@testfb.com");
     });
 
-    it("[GET] /api/users/:id - Debe devolver un usuario", async () => {
+    it("[GET] /api/users/:uid - Debe devolver un usuario", async () => {
         const {status, body} = await request.get(`/${test_user.id}`);
 
         expect(status).to.be.equal(200);
         expect(body.payload).to.be.an("object");
         expect(body.payload.name).to.be.equal("JUAN");
         expect(body.payload.surname).to.be.equal("PÉREZ");
-        expect(body.payload.email).to.be.equal("test@test.com");
+        expect(body.payload.email).to.be.equal("test@testfb.com");
         expect(body.payload.roles).to.be.an("array");
     });
 
-    it("[PUT] /api/users/update/:id - Debe actualizar un usuario", async () => {
+    it("[PUT] /api/users/update/:uid - Debe actualizar un usuario", async () => {
       const newUser = {
         surname: "GONZÁLEZ",
       };
@@ -49,12 +50,12 @@ describe("Test de integración Users", () => {
       expect(body.payload).to.be.an("object");
       expect(body.payload.name).to.be.equal("JUAN");
       expect(body.payload.surname).to.be.equal("GONZÁLEZ");
-      expect(body.payload.email).to.be.equal("test@test.com");
+      expect(body.payload.email).to.be.equal("test@testfb.com");
       expect(body.payload.roles).to.be.an("array");
     });
   
-    it("[DELETE] /api/users/delete/:id - Debe eliminar un usuario", async () => {
-        const { status, body } = await request.delete(`/delete/${test_user.id}`);
+    it("[DELETE] /api/users/delete/:uid - Debe eliminar un usuario", async () => {
+        const { status, body } = await request.delete(`/${test_user.id}`);
         
       expect(status).to.be.equal(200);
       expect(body.payload).to.be.an("object");
